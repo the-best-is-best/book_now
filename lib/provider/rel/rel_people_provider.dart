3de54b276169a412924be6a3fc585e15f5473e8f
the-best-is-best <michelle.raouf@outlook.com>
2021-07-26 21:03:32 +0200

    solve room bad

--- lib/provider/rel/rel_people_provider.dart
+++ lib/provider/rel/rel_people_provider.dart
@@ -1,9 +1,18 @@
-import 'package:book_now/modals/people/people_model.dart';
+import 'package:book_now/modals/rel/people/create_rel_people_model.dart';
+import 'package:book_now/modals/rooms/rooms_model.dart';
+import 'package:book_now/network/dio_helper.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
 class RelPeopleProvider with ChangeNotifier {
   int? selectedPeople;
   int? selectedTravel;
+
+  bool bones = false;
+
+  int? selectedhouseId;
+  int? selectedRoom;
+  List<RoomsModel> relRoom = [];
 
   void changeSelectedPeople(int? val) {
     selectedPeople = val;
@@ -12,5 +21,43 @@ class RelPeopleProvider with ChangeNotifier {
 
   void changeSelectedTravel(int? val) {
     selectedTravel = val;
+  }
+
+  void changeBonesState() {
+    bones = !bones;
+  }
+
+  void changeSelectedHouse(int? val) {
+    selectedhouseId = val;
+  }
+
+  Future getRooms(List<RoomsModel> room) async {
+    relRoom =
+        room.where((roomItem) => roomItem.houseId == selectedhouseId).toList();
+    notifyListeners();
+  }
+
+  void changeSelectedRoom(int? val) {
+    selectedRoom = val;
+  }
+
+  bool loading = false;
+  Future sendData(CreateRelPeopleModel createRelPeopleModel) async {
+    loading = true;
+    notifyListeners();
+    var createRelPeople = createRelPeopleModel.toJson();
+    var response = await DioHelper.postData(
+      url: "rel/insert_update/create_rel_people.php",
+      query: createRelPeople,
+    );
+
+    notifyListeners();
+
+    return response;
+  }
+
+  Future loadingEnd() async {
+    loading = false;
+    notifyListeners();
   }
 }
