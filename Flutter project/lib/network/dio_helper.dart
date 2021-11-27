5a36976f28314ba9574927820c84a2d79897a388
the-best-is-best <michelle.raouf@outlook.com>
2021-11-27 12:02:18 +0200

    solve files

--- Flutter project/lib/network/dio_helper.dart
+++ Flutter project/lib/network/dio_helper.dart
@@ -5,17 +5,18 @@ class DioHelper {
   static late Dio sendMessage;
 
   static init() {
+/*
     dio = Dio(BaseOptions(
       baseUrl: 'http://192.168.1.2/book_now/',
       receiveDataWhenStatusError: true,
-    ));
+    ));*/
 
     //online
 
-    // dio = Dio(BaseOptions(
-    //   baseUrl: 'http://tbib.onlinewebshop.net/book_now/',
-    //   receiveDataWhenStatusError: true,
-    // ));
+     dio = Dio(BaseOptions(
+       baseUrl: 'http://tbib.onlinewebshop.net/book_now/',
+       receiveDataWhenStatusError: true,
+     ));
 
     sendMessage = Dio(BaseOptions(
       baseUrl: 'https://fcm.googleapis.com/fcm/send',
